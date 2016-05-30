c3155dbda05e1b2fdf206dcd265d8f7da70a96f8
elinxer <654753115@qq.com>
2016-05-30 16:52:32 +0800

    添加在window-cmd下运行php方法说明文件

---
+++ php-tools/windows-cmd.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+在windows cmd 模式下运行php方式以及注意要点
+首先在系统环境变量的Path中添加php.exe的路径，
+
+打开cmd
+输入php 换行
+输入
+<?php
+echo "测试";
+?>
+按ctrl+enter
+在按enter退出编辑
+
+如果可以表明php环境正确，不行就要去排查了
+
+运行一个php文本文件
+在cmd中直接按照如下即可，测试文件最好有输出啊，注意末尾是没有分号的
+php E:\www\test.php
+
+
+
+
+
+
+
+
+ */
