ca9ebe0cac34b728f930b59f6419cc1bd3cc50cd
elinxer <654753115@qq.com>
2019-11-06 09:13:07 +0800

    更新描述备注

--- php/library/function/date_function.php
+++ php/library/function/date_function.php
@@ -4,6 +4,7 @@
 function date_rang($start_date, $end_date){
     // array_map用法：第一个参数为处理方法，第二个为需要处理的传入数组
     // 该处：使用range获取时间戳数组列表，传入后转为指定日期格式
+    // range方法说明：range 开始，结束，然后第三个参数是步长，步长用于开始到结束计算每个步的长度，达到顺序生成
     return array_map(function($n) {
         return date('Y-m-d', $n);
         }, range(strtotime($start_date), strtotime($end_date), 24 * 3600)
