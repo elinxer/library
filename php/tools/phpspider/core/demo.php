9b5037749cbad714413cb3d314a3d8251a878374
elinxer <654753115@qq.com>
2020-03-09 15:40:23 +0800

    新增下载demo

--- php/tools/phpspider/core/demo.php
+++ php/tools/phpspider/core/demo.php
@@ -11,3 +11,38 @@ $request = requests::get('http://www.mafengwo.cn/mdd/ajax_photolist.php?act=getP
 
 $selector = selector::select($request, '//li/a/img/@src');
 var_dump($selector);
+
+
+/*
+
+$spider->on_handle_img = function($fieldname, $img)
+{
+    $regex = '/src="(https?:\/\/.*?)"/i';
+    preg_match($regex, $img, $rs);
+    if (!$rs)
+    {
+        return $img;
+    }
+
+    $url = $rs[1];
+    $img = $url;
+
+    //$pathinfo = pathinfo($url);
+    //$fileext = $pathinfo['extension'];
+    //if (strtolower($fileext) == 'jpeg')
+    //{
+        //$fileext = 'jpg';
+    //}
+    //// 以纳秒为单位生成随机数
+    //$filename = uniqid().".".$fileext;
+    //// 在data目录下生成图片
+    //$filepath = PATH_ROOT."/images/{$filename}";
+    //// 用系统自带的下载器wget下载
+    //exec("wget -q {$url} -O {$filepath}");
+
+    //// 替换成真是图片url
+    //$img = str_replace($url, $filename, $img);
+    return $img;
+};
+
+ */
